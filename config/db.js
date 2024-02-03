ab31e3aae1b855e7c2df520bf24e9cc2629a0a88
rushi372000 <122743375+rushi372000@users.noreply.github.com>
2024-02-03 23:41:18 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,12 +4,12 @@ import colors from "colors";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Connected successfully with mongodb ${conn.connection.host}`.bgMagenta
-        .white
-    );
+    // console.log(
+    //   `Connected successfully with mongodb ${conn.connection.host}`.bgMagenta
+    //     .white
+    // );
   } catch (error) {
-    console.log(`Error in MongoDB. ${error}`.bgRed.white);
+    //console.log(`Error in MongoDB. ${error}`.bgRed.white);
   }
 };
 
